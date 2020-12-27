f7773ad09aa9dd254ff81b21a0d4d103c1b82389
litao1104 <litao1104@gmail.com>
2020-12-27 21:26:48 +0800

    q7-6.asm

---
+++ q7-6.asm
@@ -0,0 +1,31 @@
+assume cs:codesg, ds:datasg
+
+datasg segment
+db '1. file         '
+db '2. edit         '
+db '3. search       '
+db '4. view         '
+db '5. options      '
+db '6. help         '
+datasg ends
+
+codesg segment
+start:
+
+    mov ax, datasg
+    mov ds, ax
+
+    mov bx, 0
+    mov cx, 6
+
+s:  mov al, 3[bx]
+    and al, 11011111b
+    mov 3[bx], al
+    add bx, 16
+    loop s
+
+    mov ax, 4c00h
+    int 21h
+
+codesg ends
+end start
