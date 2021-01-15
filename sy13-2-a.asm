d2cf1ca3b50c486a0e22d4979a347fc60d5c2b2b
litao1104 <litao1104@gmail.com>
2021-01-15 23:53:54 +0800

    sy13-2-a.asm and sy13-2-i.asm

---
+++ sy13-2-a.asm
@@ -0,0 +1,26 @@
+assume cs:code
+
+code segment
+
+start:
+    mov ax, 0B800H
+    mov es, ax
+    mov di, 160 * 12
+
+    mov bx, offset s - offset se
+    mov cx, 80
+
+s:
+    mov byte ptr es:[di], '!'
+    add di, 2
+    int 7ch
+
+se:
+    nop
+
+    mov ax, 4C00H
+    int 21H
+
+code ends
+
+end start
